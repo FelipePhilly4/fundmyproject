b7924f4ac54a03fe8c50dd4f33156232fa645851
Felipe <phillybadi@gmail.com>
2020-01-23 09:43:21 +0100

    Maj DefaultController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Contribution;
 use App\Entity\Project;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,10 +17,16 @@ class DefaultController extends AbstractController
     public function index()
     {
         $projects = $this->getDoctrine()->getRepository(Project::class)->findAll();
+        $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
+        $contributions = $this->getDoctrine()->getRepository(Contribution::class)->findAll();
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
 
 
         return $this->render('default/index.html.twig', [
             'projects' => $projects,
+            'categories'=> $categories,
+            'contributions'=> $contributions,
+            'users'=> $users
         ]);
     }
 }
